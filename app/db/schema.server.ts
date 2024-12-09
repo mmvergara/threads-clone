afa9fb0745c44e7ff9bec1ae62723ab39cb67158
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-09 19:32:28 +0800

    refactor: update user schema to enforce not null constraints and change ID type; enhance HomePage loader for user authentication

--- app/db/schema.server.ts
+++ app/db/schema.server.ts
@@ -3,18 +3,20 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 // User Table
 export const users = sqliteTable("users", {
-  id: integer("id").notNull().primaryKey(),
+  id: text("id").notNull().primaryKey(),
   handle: text("handle").unique().notNull(),
   email: text("email").unique().notNull(),
   passwordHash: text("password_hash").notNull(),
   displayName: text("display_name").notNull(),
-  bio: text("bio").default("Wow, such empty!"),
-  profileImageUrl: text("profile_image_url").default(
-    "https://via.placeholder.com/300x300"
-  ),
-  followers: integer("followers").default(0),
-  following: integer("following").default(0),
-  isVerified: integer("is_verified", { mode: "boolean" }).default(false),
+  bio: text("bio").notNull().default("Wow, such empty!"),
+  profileImageUrl: text("profile_image_url")
+    .notNull()
+    .default("https://via.placeholder.com/300x300"),
+  followers: integer("followers").notNull().default(0),
+  following: integer("following").notNull().default(0),
+  isVerified: integer("is_verified", { mode: "boolean" })
+    .notNull()
+    .default(false),
   createdAt: integer("created_at", { mode: "number" })
     .notNull()
     .default(sql`(unixepoch())`),
--- app/routes/app.home.tsx
+++ app/routes/app.home.tsx
@@ -1,5 +1,13 @@
-const HomePage = ()=> {
-  return <>Homepagwwe</>
-}
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { requireUser } from "~/session/session.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await requireUser(request);
+  return null;
+};
+
+const HomePage = () => {
+  return <>HOME PAGE</>;
+};
 
 export default HomePage;
