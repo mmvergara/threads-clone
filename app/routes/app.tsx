b58c4a2462043081417e43182481338a4ca84d86
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-10 07:28:23 +0800

    refactor: adjust MainLayout structure and styling; reposition TopBar and update main content area for improved layout and responsiveness

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -7,9 +7,9 @@ const MainLayout = () => {
   return (
     <div className="flex flex-col sm:flex-row max-h-screen min-h-screen bg-[#0a0a0a] ">
       <Sidebar />
-      <div className="flex-1 overflow-y-auto ">
-        <TopBar />
-        <main className="flex flex-row justify-center gap-4 overflow-y-auto sm:border-[0.5px] border-t-[1px]  border-[#303030] sm:bg-[#181818] max-w-[700px] mx-auto sm:rounded-3xl">
+      <TopBar />
+      <div className="flex-1 overflow-y-auto mt-14">
+        <main className="flex flex-row justify-center gap-4 overflow-hidden  sm:border-[0.5px] border-t-[1px]  border-[#303030] sm:bg-[#181818] max-w-[650px] text-sm mx-auto sm:rounded-3xl">
           <Outlet />
         </main>
       </div>
