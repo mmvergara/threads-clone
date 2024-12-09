b8576ca65c48e572b7d0318e2cbbc40a6fb8375b
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-10 07:00:13 +0800

    feat: enhance MainLayout with responsive design; integrate Sidebar, TopBar, and BottomBar components for improved user experience

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,18 +1,20 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
-import { requireUser } from "~/session/session.server";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await requireUser(request);
-  return null;
-};
+import BottomBar from "~/components/layout/bottombar";
+import Sidebar from "~/components/layout/sidebar";
+import TopBar from "~/components/layout/topbar";
 
 const MainLayout = () => {
   return (
-    <>
-      layout
-      <Outlet />
-    </>
+    <div className="flex flex-col sm:flex-row max-h-screen min-h-screen bg-[#0a0a0a] ">
+      <Sidebar />
+      <div className="flex-1 overflow-y-auto ">
+        <TopBar />
+        <main className="flex flex-row justify-center gap-4 overflow-y-auto sm:border-[0.5px] border-t-[1px]  border-[#303030] sm:bg-[#181818] max-w-[700px] mx-auto sm:rounded-3xl">
+          <Outlet />
+        </main>
+      </div>
+      <BottomBar />
+    </div>
   );
 };
 
