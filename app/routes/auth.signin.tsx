c38642555896d809db34179909b53caf6af60a88
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-09 19:32:49 +0800

    feat: implement sign-in functionality with user authentication and session management

--- app/routes/auth.signin.tsx
+++ app/routes/auth.signin.tsx
@@ -1,15 +1,74 @@
-import { useState } from "react";
-import { Form, Link } from "@remix-run/react";
-import { AtSign, Lock, Eye, EyeOff } from "lucide-react";
+import { Form, Link, useActionData, useNavigate } from "@remix-run/react";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+  TypedResponse,
+} from "@remix-run/node";
+import { z } from "zod";
+import { useEffect } from "react";
+import {
+  actionError,
+  ActionReturnType,
+  handleErrorAction,
+  useToastedAction,
+} from "~/utils/action-utils";
+import { getUserByEmail } from "~/db/repo_user";
+import bcrypt from "bcrypt";
+import {
+  getUserIdFromSession,
+  storeUserInSession,
+} from "~/session/session.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const userId = await getUserIdFromSession(request);
+  if (userId) {
+    return redirect("/app");
+  }
+  return null;
+};
+
+const signInSchema = z.object({
+  email: z.string().email("Please enter a valid email"),
+  password: z.string().min(8, "Password must be 8 or more characters"),
+});
+
+export const action = async ({
+  request,
+}: ActionFunctionArgs): Promise<
+  ActionReturnType<void> | TypedResponse<unknown>
+> => {
+  try {
+    const formData = await request.formData();
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const valid = await signInSchema.parseAsync({ email, password });
+
+    const user = await getUserByEmail(valid.email);
+    if (!user) return actionError("Invalid Credentials");
+
+    const validPassword = await bcrypt.compare(password, user.passwordHash);
+    if (!validPassword) return actionError("Invalid Credentials");
+
+    const sessionHeader = await storeUserInSession(user.id);
+    return redirect("/app", {
+      headers: {
+        "Set-Cookie": sessionHeader,
+      },
+    });
+  } catch (error) {
+    return handleErrorAction(error);
+  }
+};
 
 const ThreadsSignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const data = useActionData<typeof action>();
+  useToastedAction(data as ActionReturnType<void>);
 
   return (
     <div className="min-h-screen bg-[#101010] text-white flex items-center justify-center px-4">
-      <div className="w-full max-w-sm  rounded-lg shadow-lg p-6">
-        {/* Threads-like logo */}
+      <div className="w-full max-w-sm rounded-lg shadow-lg p-6">
         <div className="text-center mb-8">
           <h1 className="text-4xl font-bold tracking-tight text-white">
             Threads
@@ -18,46 +77,32 @@ const ThreadsSignIn = () => {
             Not really, this is just a clone.
           </p>
         </div>
-
         <Form method="post" className="space-y-2">
           <input
             type="email"
             name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email or username"
+            placeholder="Email"
             required
-            className="w-full text-sm p-4 bg-[#1E1E1E]  rounded-xl
-                         focus:outline-none focus:ring-[1px] focus:ring-zinc-500 text-white placeholder-gray-400"
+            className="w-full text-sm p-4 bg-[#1E1E1E] rounded-xl focus:outline-none focus:ring-[1px] focus:ring-zinc-500 text-white placeholder-gray-400"
           />
-
-          <div className="relative">
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-              className="w-full text-sm p-4 bg-[#1E1E1E]  rounded-xl
-                         focus:outline-none focus:ring-[1px] focus:ring-zinc-500 text-white placeholder-gray-400"
-            />
-          </div>
-
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+            className="w-full text-sm p-4 bg-[#1E1E1E] rounded-xl focus:outline-none focus:ring-[1px] focus:ring-zinc-500 text-white placeholder-gray-400"
+          />
           <button
             type="submit"
-            className="w-full py-3 bg-white text-black font-semibold rounded-xl
-                       hover:bg-blue-500 transition duration-300"
+            className="w-full py-3 bg-white text-black font-semibold rounded-xl hover:bg-blue-500 transition duration-300"
           >
             Log In
           </button>
         </Form>
-
         <div className="text-center mt-6 space-y-4">
           <a href="#" className="text-zinc-500 text-sm hover:underline">
             Forgot password?
           </a>
-
           <div className="flex items-center justify-center mt-4">
             <span className="text-gray-500">Don't have an account?</span>
             <Link
