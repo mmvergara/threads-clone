110787147150c079feb08ec9816310a22f10f42c
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-09 19:32:43 +0800

    feat: add user session management functions to retrieve and require user authentication

--- app/session/session.server.ts
+++ app/session/session.server.ts
@@ -1,4 +1,5 @@
-import { createCookieSessionStorage } from "@remix-run/node";
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
+import { getUserById } from "~/db/repo_user";
 
 export const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
@@ -17,5 +18,26 @@ export const storeUserInSession = async (userId: string) => {
   const session = await getSession();
   session.set("userId", userId);
   const header = await commitSession(session);
+  
   return header;
+};
+
+export const getUserIdFromSession = async (request: Request) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  const userId = session.get("userId");
+  return userId;
+};
+
+export const requireUser = async (request: Request) => {
+  const userId = await getUserIdFromSession(request);
+  if (!userId) {
+    console.log("No userId in session, redirecting to /auth/signin ====");
+    throw redirect("/auth/signin");
+  }
+  const user = await getUserById(userId);
+  if (!user) {
+    console.log("User not found, redirecting to /auth/signin");
+    throw redirect("/auth/signin");
+  }
+  return user.id;
 };
