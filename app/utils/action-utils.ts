28364871d9390d667ab0080ac5863ed489b9618f
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-09 19:32:40 +0800

    refactor: update action utility functions to use ActionReturnType for better type safety

--- app/utils/action-utils.ts
+++ app/utils/action-utils.ts
@@ -8,10 +8,9 @@ export type ActionReturnType<T> = {
   success: boolean;
 };
 
-export const useToastedAction = (data?: {
-  message?: string[];
-  success?: boolean;
-}) => {
+export const useToastedAction = (
+  data?: ActionReturnType<unknown> | undefined
+) => {
   useEffect(() => {
     if (data?.message) {
       data.message.forEach((message) => {
@@ -21,20 +20,26 @@ export const useToastedAction = (data?: {
   }, [data]);
 };
 
-export const handleErrorAction = (error: unknown) => {
+export const handleErrorAction = (error: unknown): ActionReturnType<never> => {
   if (error instanceof z.ZodError) {
     console.error("Validation errors:", error.errors);
-    return { success: false, message: error.errors.map((e) => e.message) };
+    return {
+      success: false,
+      message: error.errors.map((e) => e.message),
+    };
   } else {
     console.error("Unexpected error:", error);
-    return { success: false, message: ["Something went wrong 😥"] };
+    return {
+      success: false,
+      message: ["Something went wrong 😥"],
+    };
   }
 };
 
-export const actionSuccess = (message: string) => {
+export const actionSuccess = (message: string): ActionReturnType<void> => {
   return { success: true, message: [message] };
 };
 
-export const actionError = (message: string) => {
+export const actionError = (message: string): ActionReturnType<never> => {
   return { success: false, message: [message] };
 };
