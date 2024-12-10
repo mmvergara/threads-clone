7094121338e9ecf611c0de040d9950e54483af4d
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-10 10:13:38 +0800

    feat: add threads table to database schema; include fields for user association, content, and metadata for improved thread management

--- app/db/schema.server.ts
+++ app/db/schema.server.ts
@@ -1,5 +1,10 @@
-import { sql } from "drizzle-orm";
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { relations, sql } from "drizzle-orm";
+import {
+  sqliteTable,
+  text,
+  integer,
+  AnySQLiteColumn,
+} from "drizzle-orm/sqlite-core";
 
 // User Table
 export const users = sqliteTable("users", {
@@ -17,6 +22,24 @@ export const users = sqliteTable("users", {
   isVerified: integer("is_verified", { mode: "boolean" })
     .notNull()
     .default(false),
+  createdAt: integer("created_at", { mode: "number" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const threads = sqliteTable("threads", {
+  id: text("id").notNull().primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  content: text("content").notNull(),
+  imageUrls: text("image_urls", { mode: "json" }).notNull(),
+  parentThreadId: text("parent_thread_id").references(
+    (): AnySQLiteColumn => threads.id
+  ),
+  likes: integer("likes").notNull().default(0),
+  replies: integer("replies").notNull().default(0),
+  reposts: integer("reposts").notNull().default(0),
   createdAt: integer("created_at", { mode: "number" })
     .notNull()
     .default(sql`(unixepoch())`),
