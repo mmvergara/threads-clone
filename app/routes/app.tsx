b76bd3288a39d8764e2d27810e70aa17768e7789
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-10 19:21:27 +0800

    refactor: improve layout structure in MainLayout component; adjust padding and margins for better responsiveness and visual consistency in the app's main content area

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -15,10 +15,12 @@ const MainLayout = () => {
     <div className="flex flex-col sm:flex-row max-h-screen min-h-screen bg-[#0a0a0a] ">
       <Sidebar />
       <TopBar />
-      <div className="flex-1 overflow-y-auto mt-14">
-        <main className="flex flex-row justify-center gap-4 overflow-hidden  sm:border-[0.5px] border-t-[1px]  border-[#303030] sm:bg-[#181818] max-w-[650px] text-sm mx-auto sm:rounded-3xl">
-          <Outlet />
-        </main>
+      <div className="flex-1 overflow-y-auto sm:pl-20 ">
+        <div className="mx-2">
+          <main className="flex mt-14 gap-4 overflow-hidden sm:border-[0.5px] border-t-[1px]  border-[#303030] sm:bg-[#181818] max-w-[650px] text-sm mx-auto sm:rounded-3xl">
+            <Outlet />
+          </main>
+        </div>
       </div>
       <BottomBar />
     </div>
