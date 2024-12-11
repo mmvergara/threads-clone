fc5741782af1eb7bda694b5ce92d9cabb2c90b87
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-11 16:38:21 +0800

    refactor: add intent parameter to updateProfileDataAction and updateProfileImgAction; streamline success message handling for improved clarity in user profile updates

--- app/.server/services/user-actions.ts
+++ app/.server/services/user-actions.ts
@@ -1,9 +1,11 @@
+import { Intent } from "~/utils/client-action-utils";
 import { handleActionSuccess } from "../utils/action-utils";
 import { updateUserProfileData, updateUserProfileImage } from "./user";
 
 export const updateProfileDataAction = async (
   userId: string,
-  formData: FormData
+  formData: FormData,
+  intent: Intent
 ) => {
   const displayName = formData.get("displayName") as string;
   const bio = formData.get("bio") as string;
@@ -11,22 +13,15 @@ export const updateProfileDataAction = async (
     displayName,
     bio,
   });
-  return handleActionSuccess(
-    "Profile updated successfully",
-    "updateProfileData",
-    user
-  );
+  return handleActionSuccess("Profile updated", intent, user);
 };
 
 export const updateProfileImgAction = async (
   userId: string,
-  formData: FormData
+  formData: FormData,
+  intent: Intent
 ) => {
   const profileImageUrl = formData.get("profileImageUrl") as string;
   const user = await updateUserProfileImage(userId, profileImageUrl);
-  return handleActionSuccess(
-    "Profile image updated successfully",
-    "updateProfileImg",
-    user
-  );
+  return handleActionSuccess("Profile image updated", intent, user);
 };
