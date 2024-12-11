235a48f676f336f335285afbd57f2d2590759575
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-11 18:37:58 +0800

    feat: add follow and unfollow user actions to user-actions service; enhance profile management with new action handlers for following and unfollowing users

--- app/.server/services/user-actions.ts
+++ app/.server/services/user-actions.ts
@@ -1,6 +1,11 @@
 import { Intent } from "~/utils/client-action-utils";
 import { handleActionSuccess } from "../utils/action-utils";
-import { updateUserProfileData, updateUserProfileImage } from "./user";
+import {
+  followUser,
+  unfollowUser,
+  updateUserProfileData,
+  updateUserProfileImage,
+} from "./user";
 
 export const updateProfileDataAction = async (
   userId: string,
@@ -24,4 +29,24 @@ export const updateProfileImgAction = async (
   const profileImageUrl = formData.get("profileImageUrl") as string;
   const user = await updateUserProfileImage(userId, profileImageUrl);
   return handleActionSuccess("Profile image updated", intent, user);
+};
+
+export const followUserAction = async (
+  currentUserId: string,
+  formData: FormData,
+  intent: Intent
+) => {
+  const toFollowUserId = formData.get("toFollowUserId") as string;
+  const user = await followUser(currentUserId, toFollowUserId);
+  return handleActionSuccess("User followed", intent, user);
+};
+
+export const unfollowUserAction = async (
+  currentUserId: string,
+  formData: FormData,
+  intent: Intent
+) => {
+  const toUnfollowUserId = formData.get("toUnfollowUserId") as string;
+  const user = await unfollowUser(currentUserId, toUnfollowUserId);
+  return handleActionSuccess("User unfollowed", intent, user);
 };
