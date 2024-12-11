51d9ee1bd6615791ab9d0e46428b2267c60bde53
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-11 09:13:27 +0800

    fix: update schema path in drizzle configuration; change schema reference from 'schema.server.ts' to 'schema.ts' for consistency in database configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./app/.server/db/schema.server.ts",
+  schema: "./app/.server/db/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
