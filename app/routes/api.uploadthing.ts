35ea56451d564f3db6e12a6524ecc8434fe86f2a
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-11 14:34:26 +0800

    refactor: remove middleware for user authentication in upload routes; streamline upload process for thread and profile images, and add TODO for future user login check implementation

--- app/routes/api.uploadthing.ts
+++ app/routes/api.uploadthing.ts
@@ -1,4 +1,3 @@
-import type { ActionFunctionArgs } from "@remix-run/node"; // or cloudflare/deno
 import { createUploadthing, type FileRouter } from "uploadthing/remix";
 import { UploadThingError } from "uploadthing/server";
 import { getUserIdFromSession } from "~/.server/session/session";
@@ -17,49 +16,21 @@ const f = createUploadthing({
   },
 });
 
-const auth = async ({ request }: ActionFunctionArgs) => {
-  return await getUserIdFromSession(request);
-};
-
-// FileRouter for your app, can contain multiple FileRoutes
+// TODO: Add middleware to check if user is logged in
 const uploadRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   threadImageUploader: f({
     image: {
       maxFileSize: "4MB",
       maxFileCount: 5,
     },
-  })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ event }) => {
-      // This code runs on your server before upload
-      const userId = await auth(event);
-
-      // If you throw, the user will not be able to upload
-      if (!userId) throw new UploadThingError("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return {};
-    })
-    .onUploadComplete(async () => {}),
+  }).onUploadComplete(async () => {}),
 
   profileImageUploader: f({
     image: {
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
-  })
-    .middleware(async ({ event }) => {
-      // This code runs on your server before upload
-      const userId = await auth(event);
-
-      // If you throw, the user will not be able to upload
-      if (!userId) throw new UploadThingError("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return {};
-    })
-    .onUploadComplete(async () => {}),
+  }).onUploadComplete(async () => {}),
 } satisfies FileRouter;
 
 export type UploadRouter = typeof uploadRouter;
@@ -70,6 +41,6 @@ export const { action, loader } = createRouteHandler({
   router: uploadRouter,
   config: {
     logFormat: "pretty",
-     // logLevel: "Error",
+    // logLevel: "Error",
   },
 });
