e5aa5960d31045af3f131dc9df0a3cd4b4f9ccad
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-12 17:52:34 +0800

    style: add custom scrollbar class to MainLayout for improved user experience; enhance layout aesthetics with scrollbar-custom class in overflow container

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -15,7 +15,7 @@ const MainLayout = () => {
     <div className="flex flex-col sm:flex-row max-h-screen min-h-screen bg-[#0a0a0a] ">
       <Sidebar />
       <TopBar />
-      <div className="flex-1 overflow-y-auto sm:pl-20 ">
+      <div className="flex-1 overflow-y-auto sm:pl-20 scrollbar-custom">
         <div className="sm:mx-2">
           <main className="flex mt-14 gap-4 overflow-hidden sm:border-[1px] border-[#303030] sm:bg-[#181818] max-w-[640px] text-sm mx-auto sm:rounded-3xl min-h-[calc(100vh-100px)]">
             <Outlet />
