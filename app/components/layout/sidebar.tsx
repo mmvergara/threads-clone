13c52b53feaf0855e96e751622ac4d31dae1dc2a
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-12 19:17:05 +0800

    feat: enhance Sidebar component with dropdown for logout functionality; integrate search link and replace PinIcon with LogInIcon for improved user navigation

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -1,16 +1,21 @@
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 import {
   AlignLeftIcon,
   AtSignIcon,
   HeartIcon,
   HomeIcon,
-  PinIcon,
+  LogInIcon,
   PlusIcon,
   SearchIcon,
   User2Icon,
 } from "lucide-react";
+import { useState } from "react";
+import SubmitBtn from "../submit-btn";
+import { Intent } from "~/utils/client-action-utils";
 
 const Sidebar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     <div className="hidden sm:flex fixed h-full flex-col z-50 justify-between w-[5rem] text-[#4d4d4d] py-4 bg-[#0a0a0a] ">
       <div className="flex items-center justify-center">
@@ -20,7 +25,10 @@ const Sidebar = () => {
         <Link to="/" className="hover:bg-[#171717] py-2.5 px-4 rounded-lg">
           <HomeIcon size={28} />
         </Link>
-        <Link to="/" className="hover:bg-[#171717] py-2.5 px-4 rounded-lg">
+        <Link
+          to="/search"
+          className="hover:bg-[#171717] py-2.5 px-4 rounded-lg"
+        >
           <SearchIcon size={28} />
         </Link>
         <button className="bg-[#171717] hover:text-white py-2.5 px-4 rounded-lg">
@@ -36,9 +44,30 @@ const Sidebar = () => {
           <User2Icon size={28} />
         </Link>
       </div>{" "}
-      <div className="flex flex-col gap-8 items-center justify-center pb-5">
-        <PinIcon size={28} />
-        <AlignLeftIcon size={28} />
+      <div className="flex flex-col gap-8 items-center justify-center pb-5 relative">
+        <button
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          className="hover:bg-[#171717] p-2.5 rounded-lg"
+        >
+          <AlignLeftIcon size={28} />
+        </button>
+
+        {isDropdownOpen && (
+          <div className="absolute bottom-16 left-16 z-10 w-48 bg-[#171717] rounded-xl border-[1px] border-[#303030] shadow-lg">
+            <div className="p-2 text-red-500">
+              <Link
+                to="/api/logout"
+                className="w-full flex gap-2 items-center p-4 hover:bg-[#252525] rounded-lg"
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                }}
+              >
+                <LogInIcon size={24} />
+                Logout
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
