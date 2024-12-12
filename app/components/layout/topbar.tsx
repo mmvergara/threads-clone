472c26a5692ba00bda31dc1479270b6af944839e
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-12 17:52:22 +0800

    style: update TopBar component for improved layout and visual hierarchy; adjust background color, border styles, and z-index for better layering and user experience

--- app/components/layout/topbar.tsx
+++ app/components/layout/topbar.tsx
@@ -25,17 +25,17 @@ const TopBar = () => {
     },
   ];
   return (
-    <div className="flex fixed w-full mx-auto flex-row items-center justify-center gap-4 mt-2   p-3 rounded-3xl">
-      <button className="font-bold mb-1">For you</button>
+    <div className="flex fixed w-full z-40 bg-[#0a0a0a] flex-row items-center justify-center gap-4 p-3 border-b-[1px] sm:border-none border-[#3d3d3d]">
+      <button className="font-bold mb-1 ml-2">For you</button>
       <div className="relative">
         <button
           onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          className="bg-[#171717] p-1 rounded-full border-[0.5px] border-[#303030] hover:bg-[#252525]"
+          className="bg-[#171717] p-1 rounded-full border-[1px] border-[#303030] hover:bg-[#252525]"
         >
           <ChevronDownIcon size={16} />
         </button>
         {isDropdownOpen && (
-          <div className="absolute right-[-120px] mt-2 w-64 bg-[#171717] rounded-xl border-[0.5px] border-[#303030] shadow-lg">
+          <div className="absolute right-[-120px] mt-2 z-10 w-64 bg-[#171717] rounded-xl border-[1px] border-[#303030] shadow-lg">
             <ol className="p-2 flex flex-col gap-1">
               {links.map((link) => (
                 <Link
