381745ad90e6b998faace8ecd8341ed559b5f7de
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-13 11:49:09 +0800

    refactor: remove commented-out thread schema definition in threads service file to clean up code and improve readability

--- app/.server/services/threads.ts
+++ app/.server/services/threads.ts
@@ -132,20 +132,3 @@ export const searchThreads = async ({
   return res;
 };
 
-// export const threads = sqlit eTable("threads", {
-//   id: text("id").notNull().primaryKey(),
-//   userId: text("user_id")
-//     .notNull()
-//     .references(() => users.id),
-//   content: text("content").notNull(),
-//   imageUrls: text("image_urls", { mode: "json" }).notNull(),
-//   parentThreadId: text("parent_thread_id").references(
-//     (): AnySQLiteColumn => threads.id
-//   ),
-//   likes: integer("likes").notNull().default(0),
-//   replies: integer("replies").notNull().default(0),
-//   reposts: integer("reposts").notNull().default(0),
-//   createdAt: integer("created_at", { mode: "number" })
-//     .notNull()
-//     .default(sql`(unixepoch())`),
-// });
