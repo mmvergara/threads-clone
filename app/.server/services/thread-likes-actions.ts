ba0587d4d8619e23bc98724b218dce96a363de06
mmvergara <mark.jesusmanabat@gmail.com>
2024-12-13 11:48:49 +0800

    feat: implement repost and unrepost actions for threads, enhancing user engagement and interaction

--- app/.server/services/thread-likes-actions.ts
+++ app/.server/services/thread-likes-actions.ts
@@ -1,5 +1,11 @@
-import { likeThread, unlikeThread } from "./thread-likes";
-import { handleActionSuccess } from "../utils/action-utils";
+import {
+  hasUserRepostedThread,
+  likeThread,
+  repostThread,
+  unlikeThread,
+  unrepostThread,
+} from "./thread-likes";
+import { handleActionError, handleActionSuccess } from "../utils/action-utils";
 import { Intent } from "~/utils/client-action-utils";
 
 export const likeThreadAction = async (
@@ -20,4 +26,30 @@ export const unlikeThreadAction = async (
   const threadId = formData.get("threadId") as string;
   const thread = await unlikeThread(threadId, userId);
   return handleActionSuccess("", intent, thread);
+};
+
+export const repostThreadAction = async (
+  currentUserId: string,
+  formData: FormData,
+  intent: Intent
+) => {
+  const threadId = formData.get("threadId") as string;
+
+  const hasUserReposted = await hasUserRepostedThread(threadId, currentUserId);
+  if (hasUserReposted) {
+    return handleActionError(["You already reposted this thread"], intent);
+  }
+
+  const thread = await repostThread(threadId, currentUserId);
+  return handleActionSuccess("Thread reposted", intent, thread);
+};
+
+export const unrepostThreadAction = async (
+  currentUserId: string,
+  formData: FormData,
+  intent: Intent
+) => {
+  const threadId = formData.get("threadId") as string;
+  const thread = await unrepostThread(threadId, currentUserId);
+  return handleActionSuccess("Thread unreposted", intent, thread);
 };
