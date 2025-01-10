3998d0db05483d2e65b565263c3db173dde2b567
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-10 09:12:59 +0800

    remove action utils

--- app/utils/client-action-utils.ts
+++ app/utils/client-action-utils.ts
@@ -4,11 +4,6 @@ import { ActionReturnType } from "~/.server/utils/action-utils";
 export enum Intent {
   UpdateProfileData = "updateProfileData",
   UpdateProfileImage = "updateProfileImage",
-
-  FollowUser = "followUser",
-  UnfollowUser = "unfollowUser",
-
-  UnrepostThread = "unrepostThread",
 }
 
 export const useUniversalActionData = () => {
